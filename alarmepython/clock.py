8e9b60a8ff9ec7a476517300b4d4404adf221ba0
MatheusLeao <mathegiov@hotmail.com>
2019-11-29 15:55:07 -0300

    erros nos inputs

--- alarmepython/clock.py
+++ alarmepython/clock.py
@@ -1,5 +1,4 @@
 import datetime
-#import playsound
 from pygame import mixer
 
 horas = int(input("Que horas tocar o alarme?\n"))
@@ -13,6 +12,7 @@ print("Aguardando")
 
 while True:
     if(horas == datetime.datetime.now().hour and minuto == datetime.datetime.now().minute):
-       print("Tocando agora as " + str(datetime.datetime.now()))
-       #playsound.playsound('/path/to/filename.mp3', True)
+       mixer.init()
+       mixer.music.load('/home/matheusleao/arquivos_importantes/alarme_escolar/alarmepython/sinal.mp3')
+       mixer.music.play()
        break
