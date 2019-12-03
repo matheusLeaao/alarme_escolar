bbd53111b775874af20a3b6d60060f96f8103d9f
MatheusLeao <mathegiov@hotmail.com>
2019-12-03 10:02:09 -0300

    import nas libs com erro

--- alarmepython/clock.py
+++ alarmepython/clock.py
@@ -3,7 +3,7 @@ from pygame import mixer
 
 horas = int(input("Que horas tocar o alarme?\n"))
 minuto = int(input("Que minuto tocar o alarme?\n"))
-periodo = str(input("Digite am ou pm?\n"))
+periodo = input("Digite am ou pm?\n")
 
 if(periodo == "pm"):
     horas = horas + 12
@@ -12,7 +12,4 @@ print("Aguardando")
 
 while True:
     if(horas == datetime.datetime.now().hour and minuto == datetime.datetime.now().minute):
-       mixer.init()
-       mixer.music.load('/home/matheusleao/arquivos_importantes/alarme_escolar/alarmepython/sinal.mp3')
-       mixer.music.play()
-       break
+       pygame.mixer.music.queue('sinal.mp3')
