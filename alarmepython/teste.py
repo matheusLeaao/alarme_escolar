b692c274f3e1b23569d167bbdb535ca8271dfeb9
M. Leão da C. Pereira <mathegiov@hotmail.com>
2020-02-17 10:45:07 -0300

    Update teste.py

--- alarmepython/teste.py
+++ alarmepython/teste.py
@@ -1,4 +1,13 @@
+#!bin/bash
+# Teste tocar música python 
+#
+# Github : matheusLeaao  <https://github.com/matheusLeaao>
+# @author Matheus Leão <mathegiov@hotmail.com>
+
+
 import pygame
+
+
 
 pygame.init()
 pygame.mixer.music.load('sinal.mp3')
